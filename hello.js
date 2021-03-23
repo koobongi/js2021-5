73358cc260d4da6c6029745a88a7709565e07d50
KBG <koobongi@naver.com>
2021-03-23 16:18:10 +0900

    메모수정

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
-let h = new Date();
+let date = new Date();
 h = date.getHours();
 
 console.log(h < 3 || h > 8);
 console.log(h >= 3 && h <= 8);
+
+let type = typeof(date.getHours());
+console.log(type);
