619a8e873637d7df2c28c65f1ddca3a60ae71e3f
KBG <koobongi@naver.com>
2021-03-23 16:43:17 +0900

    0323메모

--- hello.js
+++ hello.js
@@ -6,3 +6,28 @@ console.log(h >= 3 && h <= 8);
 
 let type = typeof(date.getHours());
 console.log(type);
+
+const con1 = "상수선언";
+console.log(con1);
+
+// 홀짝
+let input = 31;
+
+if ( input % 2 == 0){
+    console.log("짝수!");
+}
+console.log("홀수!");
+
+//
+if ( date.getHours() < 12 ){
+    console.log("오전!");
+}
+console.log("오후!");
+
+// Date()
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getUTCMinutes());
+console.log(date.getSeconds());
