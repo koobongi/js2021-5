e71c21e8481e9fdbc9a0dcf74b541c4f4261f547
KBG <koobongi@naver.com>
2021-03-30 15:45:53 +0900

    메모추가

---
+++ c3-18.js
@@ -0,0 +1,14 @@
+const repl = require('repl');
+
+repl.start({
+    prompt: "숫자 입력> ",
+    eval: (command, context, filename, caooback) => {
+        let number = Number(command);
+        if (isNaN(number)) {
+            console.log("숫자가 아닙니다.");
+        } else {
+            console.log("숫자입니다.");
+        }
+        callback();
+    }
+});
