c583b6f86c08be5ff16e258b93a5e35a56a61942
KBG <koobongi@naver.com>
2021-04-06 16:46:15 +0900

    메모추가

--- 4-7.js
+++ 4-7.js
@@ -1,9 +1,21 @@
 let output = "";
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < i+1; j++) {
-        output += "*";
+for (let i = 1; i <= 5; i++) {
+    for (let j = 5; j >i; j--) {
+        output += "";
+    }
+    for (let j =1; j<(i*2); j++) {
+        output += "*"
     }
     output += "\n";
+}
+for (let s = 1; s <= 4; s++) {
+    for (let d= 0; d <s; d++) {
+        output += " ";
+    }
+    for (let a=9; a>s*2; a--) {
+        output += "*";
+    }
+    output +="\n";
 }
 console.log(output);
