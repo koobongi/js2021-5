e95e455691d11b7c3bb59839bc97c400a27aa0e2
koobongi <koobongi@naver.com>
2021-05-04 16:50:29 +0900

    추가

--- ch07.js
+++ ch07.js
@@ -1,9 +1,36 @@
-function Construtor() {}
-Construtor.foo = 273;
-Construtor.bar = function () {}
+let foo = "좋은 아침입니다.";
 
-console.log(Construtor.foo);
-console.log(Construtor.bar);
+if (foo.indexOf('아침')>=0) {
+    console.log("굿모닝");
+}
+else {
+    console.log("굿나잇");
+}
+
+
+//
+// let foo = "abcdefg";
+// let bar = foo.toUpperCase();
+// let foobar = bar.toLowerCase();
+// console.log(foo);
+// console.log(bar);
+// console.log(foobar);
+
+
+//
+// let foo = Number.MAX_VALUE;
+// let bar = Number.MAX_VALUE+1;
+
+// console.log(foo);
+// console.log(bar);
+
+//
+// function Construtor() {}
+// Construtor.foo = 273;
+// Construtor.bar = function () {}
+
+// console.log(Construtor.foo);
+// console.log(Construtor.bar);
 
 
 //
