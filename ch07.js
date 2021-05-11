70af1c1446c7f5cdb24f44447de096cb59666229
koobongi <koobongi@naver.com>
2021-05-11 16:46:50 +0900

    메모추가

--- ch07.js
+++ ch07.js
@@ -1,11 +1,64 @@
 //
-let now = new Date();
-let before = new Date("Dec 9, 2020");
+let foo = [55, 65, 400, 150, 24];
 
-let interval = now.getTime() - before.getTime();
-interval = Math.floor(interval / (1000 * 60 * 60 * 24));
+foo.forEach((item, index) => {
+    console.log(`${index} - ${item}`);
+});
+console.log("=================");
+let bar = foo.map((item, index) => {
+    return item + 10;
+});
+console.log(bar);
+console.log("=================");
+let foobar = foo.filter((item, index) => {
+    return item % 2 == 0; // 짝수만 return // !== 홀수만 return
+});
+console.log(foobar);
 
-console.log(interval);
+//
+// let foo = [10, 3, 5, 20];
+// let foo = [
+//     {
+//         // key: value,
+//         name: '고구마',
+//         price: 1000,
+//     },
+//     {
+//         name: '감자',
+//         price: 500,
+//     },
+//     {
+//         name: '바나나',
+//         price: 1500,
+//     },
+// ];
+
+// let popped = foo.pop();
+// console.log(popped);
+// console.log(foo);
+// console.log();
+
+// foo.push(popped);
+// foo.push(
+//     {
+//         name: "사과",
+//         price: 2000
+//     },
+//     {
+//         name: "수박",
+//         price: 3000
+//     }
+// );
+// console.log(foo);
+
+//
+// let now = new Date();
+// let before = new Date("Dec 9, 2020");
+
+// let interval = now.getTime() - before.getTime();
+// interval = Math.floor(interval / (1000 * 60 * 60 * 24));
+
+// console.log(interval);
 
 
 //
